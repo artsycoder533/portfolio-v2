3cbfb52c37a6cc56bc833f6c16bd9d00d5982ab7
Natasha Johnson <njohnson533@gmail.com>
2023-11-19 21:49:20 -0500

    chore: updated project data

--- app/utils/data.js
+++ app/utils/data.js
@@ -1,33 +1,35 @@
 import omni from "../../public/images/omni.png";
-import vohps from "../../public/images/vohps.jpg"
-import atlas from "../../public/images/atlas.png"
+import vohps from "../../public/images/vohps.jpg";
+import atlas from "../../public/images/atlas.png";
+import renewed from "../../public/images/renewed.png";
 
 
 export const projects = [
+    {
+        title: "ReNEWed Psychological and Consultation Services", 
+        tags: ["Freelance", "Re-Brand"],
+        img: renewed,
+        description: "A multi-page static website for a mental health agency out of Suprise, AZ. They wanted to leave the Wix Platform, so I built a rebrand of their original website from scratch.",
+        technologies: "Next.js 13.3 Pages Router and Tailwind",
+        siteURL: "https://renewedpsychological.com/",
+        github: "https://github.com/artsycoder533/renewed",
+    },
     {
         title: "Omni Addiction & Mental Health Services", 
         tags: ["Freelance"],
         img: omni,
         description: "A single page static website for a local mental health agency out of Lawrenceville, VA. They needed a website to attract new clients as well as provide a way for them to schedule appointments online.",
-        technologies: "Next.js 13, Tailwind, Framer Motion and React Calendly",
+        technologies: "Next.js 13.1 Pages Router, Tailwind, Framer Motion and React Calendly",
         siteURL: "https://omniamh.com/",
         github: "https://github.com/artsycoder533/omni",
     },
-    {
-        title: "ReNEWed Psychological and Consultation Services", 
-        tags: ["Freelance", "Re-Brand"],
-        img: omni,
-        description: "A multi-page static website for a mental health agency out of Suprise, AZ. They wanted to leave the Wix Platform, so I built a rebrand of their original website from scratch.",
-        technologies: "Next.js 13.5 and Tailwind",
-        siteURL: "https://renewedpsychological.com/",
-        github: "https://github.com/artsycoder533/renewed",
-    },
+    
     {
         title: "Visions of Hope Psychological Services", 
         tags: ["Freelance", "Re-Brand"],
         img: vohps,
         description: "A single page static website for a local mental health agency out of Lawrenceville, VA. They needed a website to attract new clients as well as provide a way for them to schedule appointments online.",
-        technologies: "Next.js 12, Styled Components",
+        technologies: "Next.js 12.3, Styled Components",
         siteURL: "https://visionsofhope-va.com/",
         github: "https://github.com/artsycoder533/vohps-next",
     },
