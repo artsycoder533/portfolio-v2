b61f39ed8187cfa4339b97ec85d0fcc0fd390dd8
Natasha Johnson <njohnson533@gmail.com>
2024-01-01 00:35:44 -0500

    chore: update hover color

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -1,38 +1,42 @@
 import React from "react";
-import Image from "next/image";
-import headshot from "../../public/images/headshot.jpg";
 import { FaHeart } from "react-icons/fa";
 import Link from "next/link";
 
-type Props = {};
-
-const Hero = (props: Props) => {
+const Hero = () => {
   return (
     <section className=" h-[600px] flex items-center" id="home">
       <div className="max-w-[1400px] w-[90vw] mx-auto flex flex-col justify-between">
-  
-          <h1 className="text-8xl font-semibold text-primary first-letter:text-accent">
-            Natasha Johnson
-          </h1>
-          <p className="text-3xl text-primary">Software Engineer @ Township</p>
-          <p className="flex items-center gap-1 mt-4">
-            I <FaHeart className="text-red-500" /> the front end.
-          </p>
-          <div className="mt-6 flex flex-row gap-4">
-            <Link
-              href="#projects"
-              className="border bg-accent text-white rounded-lg px-4 py-3 hover:bg-secondary"
-            >
-              View Projects
-            </Link>
-            <Link
-              href="#contact"
-              className="border border-accent text-accent rounded-lg px-4 py-3 hover:text-secondary hover:border-secondary"
-            >
-              Contact
-            </Link>
-          </div>
-        
+        <h1 className="text-8xl font-semibold text-primary first-letter:text-accent">
+          Natasha Johnson
+        </h1>
+        <p className="text-3xl text-primary">
+          Software Engineer @{" "}
+          <Link
+            href="https://www.township.agency"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-accent"
+          >
+            Township
+          </Link>
+        </p>
+        <p className="flex items-center gap-1 mt-4">
+          I <FaHeart className="text-red-500" /> the front end.
+        </p>
+        <div className="mt-6 flex flex-row gap-4">
+          <Link
+            href="#projects"
+            className="border bg-accent text-white rounded-lg px-4 py-3 hover:bg-[#9E1E23]"
+          >
+            View Projects
+          </Link>
+          <Link
+            href="#contact"
+            className="border border-accent text-accent rounded-lg px-4 py-3 hover:bg-gray-200"
+          >
+            Contact
+          </Link>
+        </div>
       </div>
     </section>
   );
