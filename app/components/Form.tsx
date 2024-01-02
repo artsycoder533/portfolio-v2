775ec911eec5d1c92e6eef2048e9e225af5afb14
Natasha Johnson <njohnson533@gmail.com>
2024-01-01 20:04:52 -0500

    chore: integrate formspree

--- app/components/Form.tsx
+++ app/components/Form.tsx
@@ -1,61 +1,17 @@
 "use client"
 import React, { useState } from "react";
+import { useForm } from "@formspree/react";
 
 function Form() {
-  const [formData, setFormData] = useState({
-    name: "",
-    email: "",
-    message: "",
-  });
+  const [state, handleSubmit] = useForm(process.env.NEXT_PUBLIC_FORM as string);
 
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
-    const { name, value } = e.target;
-    setFormData((prevData) => ({
-      ...prevData,
-      [name]: value,
-    }));
-  };
-
-  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-  
-    const form = e.currentTarget;
-    const formData = new FormData(form);
-  
-    try {
-      const response = await fetch("https://formspree.io/f/moqybgqo", {
-        method: "POST",
-        body: formData,
-        headers: {
-          Accept: "application/json",
-        },
-      });
-  
-      if (response.ok) {
-        // Form submission was successful
-        console.log("Form submitted successfully");
-        // Reset the form data
-        setFormData({
-          name: "",
-          email: "",
-          message: "",
-        });
-      } else {
-        // Handle errors
-        console.error("Form submission failed");
-      }
-    } catch (error) {
-      console.error("Error submitting form:", error);
-    }
-  };
-
+  if(state.succeeded){
+    return <p>Thanks for your submission!</p>
+  }
+ 
   return (
     <form
-      name="portfoliov2"
-      action="https://formspree.io/f/moqybgqo"
-      method="POST"
       className="space-y-8 max-w-[600px] w-[90vw] mx-auto"
-      data-netlify="true"
       onSubmit={handleSubmit}
     >
       <div className="flex flex-col sm:flex-row gap-8">
@@ -67,9 +23,7 @@ function Form() {
             type="text"
             name="name"
             id="name"
-            value={formData.name}
             className="border rounded-lg p-2 bg-background"
-            onChange={handleChange}
             required
           />
         </div>
@@ -81,9 +35,7 @@ function Form() {
             type="email"
             name="email"
             id="email"
-            value={formData.email}
             className="border rounded-lg p-2 bg-background"
-            onChange={handleChange}
             required
           />
         </div>
@@ -97,13 +49,10 @@ function Form() {
           name="message"
           id="message"
           rows={6}
-          value={formData.message}
           className="border rounded-lg p-2 bg-background"
-          onChange={handleChange}
           required
         />
       </div>
-      <input type="hidden" name="portfoliov2" value="portfoliov2" />
       <button
         type="submit"
         className="bg-accent hover:bg-[#9E1E23] text-white px-4 py-3 rounded-lg"
