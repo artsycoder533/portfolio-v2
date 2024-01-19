c023bfbd4d79d39f95b08ae224f4e00e4e19582d
Natasha Johnson <njohnson533@gmail.com>
2024-01-19 00:06:52 -0500

    chore: update skills section

--- app/components/Skills.tsx
+++ app/components/Skills.tsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import { SiNextdotjs, SiTailwindcss, SiSanity} from "react-icons/si";
+import { SiNextdotjs, SiTailwindcss, SiSanity, SiTypescript} from "react-icons/si";
 import { FaReact, FaVuejs, FaShopify } from "react-icons/fa";
 
 const Skills = () => {
   return (
     <section className="py-32 bg-primary" id="skills">
         <h2 className="text-6xl mb-10 font-semibold max-w-[1400px] w-[90vw] mx-auto text-secondary">Skills.</h2>
-        <div className="max-w-[1400px] w-[90vw] mx-auto grid grid-cols-2 md:grid-cols-3 lg:grid-cols-6 justify-center text-2xl font-medium text-accent gap-16">
+        <div className="max-w-[1400px] w-[90vw] mx-auto grid grid-cols-2 md:grid-cols-3 lg:grid-cols-7 justify-center text-2xl font-medium text-accent gap-16">
             <div className="flex flex-col items-center gap-1 text-center">
                 <SiNextdotjs className="text-7xl"/>
                 <p>Next.js</p>
@@ -18,6 +18,10 @@ const Skills = () => {
             <div className="flex flex-col items-center gap-1 text-center">
                 <FaVuejs className="text-7xl"/>
                 <p>Vue</p>
+            </div>
+            <div className="flex flex-col items-center gap-1 text-center">
+                <SiTypescript className="text-7xl"/>
+                <p>TypeScript</p>
             </div>
             <div className="flex flex-col items-center gap-1 text-center">
                 <SiTailwindcss className="text-7xl"/>
